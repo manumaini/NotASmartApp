488df2052463aafaa1330a17f7254fc1566d9cf3
manav <manavmaini11@gmail.com>
2020-11-27 13:03:44 +0530

    ground work laid ;)

---
+++ app/src/main/java/com/covidapp/notasmartapp/Presenters/RegistrationPresenter.java
@@ -0,0 +1,21 @@
+package com.covidapp.notasmartapp.Presenters;
+
+import android.content.Context;
+
+import com.covidapp.notasmartapp.Interfaces.MainContract;
+
+public class RegistrationPresenter implements MainContract.RegistrationPresenter {
+
+    private Context context;
+    private MainContract.RegistrationView view;
+
+    public RegistrationPresenter(Context context, MainContract.RegistrationView view) {
+        this.context = context;
+        this.view = view;
+    }
+
+    @Override
+    public void register(String email, String password) {
+
+    }
+}

05828f31ea8786b72f036c5b2ee43beca3116de9
manav <manavmaini11@gmail.com>
2020-12-10 19:58:38 +0530

    Nearby hospitals

--- app/src/main/java/com/covidapp/notasmartapp/Views/Main/MapFragment.java
+++ app/src/main/java/com/covidapp/notasmartapp/Views/Main/MapFragment.java
@@ -1,8 +1,10 @@
 package com.covidapp.notasmartapp.Views.Main;
 
 import android.Manifest;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -38,7 +40,7 @@ import java.util.List;
 public class MapFragment extends Fragment implements MainContract.MapView {
 
     private final String TAG = "map fragment";
-    public GoogleMap map=null;
+    public GoogleMap map = null;
     private MapPresenter presenter;
     private final int FINE_LOCATION_REQ_CODE = 1;
     private FusedLocationProviderClient locationProviderClient;
@@ -47,40 +49,43 @@ public class MapFragment extends Fragment implements MainContract.MapView {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_map,container,false);
+        View view = inflater.inflate(R.layout.fragment_map, container, false);
         SupportMapFragment mapFragment = ((SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map));
-        presenter = new MapPresenter(this,getContext(),getActivity());
+        presenter = new MapPresenter(this, getContext(), getActivity());
         locationProviderClient = LocationServices.getFusedLocationProviderClient(getContext());
 
         mapFragment.getMapAsync(new OnMapReadyCallback() {
             @Override
             public void onMapReady(GoogleMap googleMap) {
-                map=googleMap;
-                Log.d(TAG, "onMapReady:value if map is : "+map);
+                map = googleMap;
+                Log.d(TAG, "onMapReady:value if map is : " + map);
 
             }
 
         });
-        
+
         getCurrentLocation();
 
         return view;
     }
-    
-    private void getCurrentLocation(){
-        if(ContextCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED){
-            ActivityCompat.requestPermissions(getActivity(),new String[]{ Manifest.permission.ACCESS_FINE_LOCATION},FINE_LOCATION_REQ_CODE);
-        }else if(ContextCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED){
-            ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.ACCESS_COARSE_LOCATION},COARSE_LOCATION_REQ_CODE);
-        }else{
+
+    private void getCurrentLocation() {
+        if (ContextCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, FINE_LOCATION_REQ_CODE);
+        } else if (ContextCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, COARSE_LOCATION_REQ_CODE);
+        } else {
             Log.d(TAG, "getCurrentLocation: all permission check");
             locationProviderClient.getLastLocation().addOnSuccessListener(new OnSuccessListener<Location>() {
                 @Override
                 public void onSuccess(Location location) {
-                    if(location != null){
+                    if (location != null) {
                         presenter.loadLocation(location);
-                    }else{
-                        Toast.makeText(getContext(),"cant get the current location",Toast.LENGTH_LONG).show();
+                    } else {
+                        Toast.makeText(getContext(), "cant get the current location", Toast.LENGTH_LONG).show();
+                        Intent intent = new Intent(android.content.Intent.ACTION_VIEW, Uri.parse("http://maps.google.com/maps"));
+                        startActivity(intent);
+                        getContext().startActivity(intent);
                     }
 
                 }
@@ -90,13 +95,13 @@ public class MapFragment extends Fragment implements MainContract.MapView {
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        switch(requestCode){
+        switch (requestCode) {
             case FINE_LOCATION_REQ_CODE:
             case COARSE_LOCATION_REQ_CODE:
-                if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                     getCurrentLocation();
-                }else{
-                    Toast.makeText(getContext(),"Permission Required",Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(getContext(), "Permission Required", Toast.LENGTH_LONG).show();
                 }
                 break;
 
@@ -106,17 +111,17 @@ public class MapFragment extends Fragment implements MainContract.MapView {
 
     @Override
     public void onSuccess(List<Result> loclist) {
-        LatLng latLng=null;
+        LatLng latLng = null;
         LatLngBounds.Builder builder = LatLngBounds.builder();
-        for(Result result : loclist){
-            latLng= new LatLng(result.getGeometry().getLocation().getLat(),result.getGeometry().getLocation().getLng());
+        for (Result result : loclist) {
+            latLng = new LatLng(result.getGeometry().getLocation().getLat(), result.getGeometry().getLocation().getLng());
             MarkerOptions options = new MarkerOptions().position(latLng).title(result.getName()).icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN));
             map.addMarker(options);
             builder.include(latLng);
 
         }
         LatLngBounds bounds = builder.build();
-        CameraUpdate cu = CameraUpdateFactory.newLatLngBounds(bounds, 25,25,1);
+        CameraUpdate cu = CameraUpdateFactory.newLatLngBounds(bounds, 25, 25, 1);
         map.animateCamera(cu);
 
 

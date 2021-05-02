0837cfbd615a1429c74c3d3b3d603f76c14b067a
Rohit <rohitbhalla53@gmail.com>
2021-05-02 14:18:44 +0530

    terms of service added and phone number added in hospital fragment

--- app/src/main/java/com/covidapp/notasmartapp/Adapters/HospitalAdapter.java
+++ app/src/main/java/com/covidapp/notasmartapp/Adapters/HospitalAdapter.java
@@ -1,18 +1,24 @@
 package com.covidapp.notasmartapp.Adapters;
 
+import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.covidapp.notasmartapp.R;
+import com.covidapp.notasmartapp.Views.Main.MainActivity;
 
 import java.util.ArrayList;
 
@@ -46,7 +52,7 @@ public class HospitalAdapter extends RecyclerView.Adapter<HospitalAdapter.Holder
         holder.name.setText(list.get(position).get(0));
         holder.address.setText(list.get(position).get(1));
         holder.beds.setText(list.get(position).get(4)+"/"+list.get(position).get(3));
-        holder.phone.setText("coming soon");
+        holder.phone.setText(list.get(position).get(5));
         holder.next.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
